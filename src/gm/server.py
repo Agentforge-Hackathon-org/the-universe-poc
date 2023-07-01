98177a22298bee5594a1dfce62a0be053da39252
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-07-01 14:00:57 -0700

    add action evaluation endpoint to game master service

--- src/gm/server.py
+++ src/gm/server.py
@@ -8,6 +8,16 @@ class Scene(BaseModel):
     scene: str
 
 
-@app.post("/narration")
+class Action(BaseModel):
+    character: str
+    action: str
+
+
+@app.post("/narrative")
 async def get_narration(scene: Scene):
-    return {"narration": "All the people are doing their shit"}
+    return {"narrative": "All the people are doing their shit"}
+
+
+@app.post("/evaluate")
+async def evaluate_action(action: Action):
+    return {"result": "This guy did some shit"}
