64f4bbb7eb7dd185fcb41b5fd1dfcfc3d7c0fd86
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-07-01 13:52:53 -0700

    add Game Master service

---
+++ src/gm/server.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Scene(BaseModel):
+    scene: str
+
+
+@app.post("/narration")
+async def get_narration(scene: Scene):
+    return {"narration": "All the people are doing their shit"}
