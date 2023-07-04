dd225bdb95a445b0a26549be2dac7eb4ff693594
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-07-04 00:58:02 -0700

    add some llm stuff

--- backend/src/storyteller/server.py
+++ backend/src/storyteller/server.py
@@ -1,5 +1,12 @@
+import os
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+import llm
+
+
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 
 app = FastAPI()
 
@@ -9,14 +16,19 @@ class ScenePrompt(BaseModel):
 
 
 class SceneUpdate(BaseModel):
-    update: str
+    current_scene: str
+    changes: str
 
 
 @app.post("/scene")
 async def create_scene(prompt: ScenePrompt):
-    return {"scene": "People be doin' shit"}
+    response = await llm.claude_complete(ANTHROPIC_API_KEY, prompt.prompt)
+    return {"scene": response}
 
 
 @app.put("/scene")
 async def update_scene(update: SceneUpdate):
-    return {"scene": "People be doin' crazy shit"}
+    prompt = f"Here is the current scene: {update.current_scene}.\n" \
+             f"Please update the scene given these changes: {update.changes}"
+    response = await llm.claude_complete(ANTHROPIC_API_KEY, prompt)
+    return {"scene": response}
